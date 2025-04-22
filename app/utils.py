21a9d80792e1b6410885ad0ab2485c064235f48c
Saichandra123456 <saichandra0703@gmail.com>
2025-04-22 14:51:34 +0530

    requirements updated

--- app/utils.py
+++ app/utils.py
@@ -22,3 +22,29 @@ def token_required(f):
     decorator.__name__ = f.__name__
     return decorator
 
+
+
+# def token_required(f):
+#     @wraps(f)
+#     def decorator(*args, **kwargs):
+#         auth_header = request.headers.get("Authorization", None)
+
+#         if not auth_header:
+#             return jsonify({"message": "Authorization header is missing"}), 401
+
+#         parts = auth_header.split()
+
+#         if len(parts) != 2 or parts[0] != "Bearer":
+#             return jsonify({"message": "Authorization header must be in the format: Bearer <token>"}), 401
+
+#         token = parts[1]
+
+#         try:
+#             jwt.decode(token, "your-jwt-secret", algorithms=["HS256"])
+#         except jwt.ExpiredSignatureError:
+#             return jsonify({"message": "Token expired"}), 401
+#         except jwt.InvalidTokenError:
+#             return jsonify({"message": "Invalid token"}), 401
+
+#         return f(*args, **kwargs)
+#     return decorator
